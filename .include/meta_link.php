446173b3d70b242ff01dab9aec632b8eeda0d512
yousuke abe <yousuke_a@mameten.sakura.ne.jp>
2017-11-23 20:12:10 +0900

    includeの削除

--- .include/meta_link.php
+++
@@ -1,8 +0,0 @@
-
-		<meta name="keywords" content=""><!--キーワードを入れてください-->
-
-
-		<meta name="viewport" content="width=800">
-		<meta name="format-detection" content="telephone=no">
-		<link rel="icon" type="image/vnd.microsoft.icon" href="favicon.ico" sizes="32x32"><!--ファビコンをリンクしてください-->
-		<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.18.1/build/cssreset/cssreset-min.css">
